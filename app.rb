6a68e906e2e50b1ba788894bf07d5565cbbddf7c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-12 15:28:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Wheeler"
+  end
+
+  get '/hometown' do
+    "My hometown is the moon"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is On Mani"
+  end
 end
